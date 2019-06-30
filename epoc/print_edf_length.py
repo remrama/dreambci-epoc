320b24de1889445137ca7ac7b633567886afa1eb
remrama <mallett.remy@gmail.com>
2019-06-30 14:42:07 +0200

    checking out old testbench files

---
+++ epoc/print_edf_length.py
@@ -0,0 +1,22 @@
+"""
+Print the length of an EPOC+ TestBench file.
+"""
+from os.path import expanduser
+from mne.io import read_raw_edf
+
+SUBJ = 'sub-003'
+SESS = 'ses-002'
+stamp = '20151115T134018'
+
+datadir = expanduser('~/DBp/proj/bcilu/EPOC/data')
+fname = f'{datadir}/{SUBJ}/{SESS}/{SUBJ}_{SESS}_testbench-{stamp}.edf'
+
+edf = read_raw_edf(fname)
+
+true_len = edf.times[-1]
+predicted_len = edf.times.shape[0] * (1/edf.info['sfreq'])
+
+assert abs(true_len-predicted_len) < 0.5
+
+print('=======================')
+print(f'EDF file is {true_len:.1f} seconds ({true_len/60:.1f} min) long.')
